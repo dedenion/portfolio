2faea67e312d6a300a86fe19e2b3e5acd145a642
dedenion <m328mitsuya@outlook.jp>
2024-01-15 16:59:34 +0900

    plus

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import "./style.css";
 import * as THREE from "three";
-
+import {GLTFLoader} from "./public/loaders/GLTFLoader"
+import { FontLoader } from "./public/threefonts/FontLoader"
+import { TextGeometry } from "./public/threefonts/TextGeometry"
 /**
  * 必須の3要素
  */
@@ -16,11 +18,24 @@ const sizes = {
 // Scene
 const scene = new THREE.Scene();
 
+//ライト
+const light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(5, 10, 5); 
+scene.add(light);
+
+
+
+
+
+
 /**
  * GridHelperの設定
  */
-const gridHelper = new THREE.GridHelper(30, 30);
-scene.add(gridHelper);
+/*const gridHelper = new THREE.GridHelper(30, 30);
+scene.add(gridHelper);*/
+
+
+
 
 // Camera
 const camera = new THREE.PerspectiveCamera(
@@ -40,13 +55,131 @@ renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 //オブジェクトの追加
-const geometry = new THREE.BoxGeometry(5, 5, 5, 10);
+const geometry = new THREE.BoxGeometry(1, 1, 1, 10);
 const material = new THREE.MeshNormalMaterial();
 
 const torus = new THREE.Mesh(geometry, material);
 torus.position.set(0, 0.5, -15);
 torus.rotation.set(1, 1, 0);
-scene.add(torus);
+//scene.add(torus);
+
+  
+// 新しい頂点シェーダー
+const newVertexShader = `
+    varying vec2 vUv;
+    uniform float time;
+
+    void main() {
+        vUv = uv;
+        vec3 newPosition = position;
+        float displacement = sin(time * 2.0 + position.x * 5.0) * 0.3; // 小さな波の振幅
+        newPosition.z += displacement;
+        gl_Position = projectionMatrix * modelViewMatrix * vec4(newPosition, 1.0);
+    }
+`;
+
+
+
+// 新しいフラグメントシェーダー
+const newFragmentShader = `
+    varying vec2 vUv;
+    uniform float time;
+
+    void main() {
+        vec3 color = vec3(0.0);
+        color.r = 0.5 + 0.5 * sin(time * 2.0); // 時間による赤色の変化
+        color.g = 0.5 + 0.5 * cos(time * 1.5); // 時間による緑色の変化
+        color.b = 0.5 + 0.5 * sin(time * 1.0); // 時間による青色の変化
+        gl_FragColor = vec4(color, 1.0);
+    }
+`;
+
+// 新しいシェーダーマテリアルの作成
+const shaderMaterial = new THREE.ShaderMaterial({
+    vertexShader: newVertexShader,
+    fragmentShader: newFragmentShader,
+    uniforms: { time: { value: 0.0 } }
+});
+
+
+
+
+//テキストオブジェクト
+const fontLoader = new FontLoader()
+fontLoader.load('/fonts/DotGothic16_Regular.json', (font) => {
+const textGeometry = new TextGeometry("My", {
+    font: font,
+    size: 1.5,
+    height: 0.2,
+    curveSegments: 12,
+    bevelEnabled: true,
+    bevelThickness: 0.03,
+    bevelSize: 0.02,
+    bevelOffset: 0,
+    bevelSegments: 5,
+})
+textGeometry.center()
+
+
+const text = new THREE.Mesh(textGeometry, shaderMaterial)
+text.castShadow = true
+text.position.x = 0;
+text.position.y = 6;
+text.position.z = -1;
+
+scene.add(text)
+})
+
+fontLoader.load('/fonts/DotGothic16_Regular.json', (font) => {
+  const textGeometry = new TextGeometry("Portfolio", {
+      font: font,
+      size: 1.5,
+      height: 0.2,
+      curveSegments: 12,
+      bevelEnabled: true,
+      bevelThickness: 0.03,
+      bevelSize: 0.02,
+      bevelOffset: 0,
+      bevelSegments: 5,
+  })
+  textGeometry.center()
+  
+  
+  const text1 = new THREE.Mesh(textGeometry, shaderMaterial)
+  text1.castShadow = true
+  text1.position.x = 0;
+  text1.position.y = 3;
+  text1.position.z = -1;
+  
+  scene.add(text1)
+  })
+
+
+
+
+//3Dオブジェクトの追加
+let earth;
+let ramen;
+
+const gltfLoader = new GLTFLoader();
+gltfLoader.load("./textures/earth/scene.gltf", (gltf) => {
+  earth = gltf.scene;
+  earth.scale.set(0.1,0.1,0.1);
+  earth.position.x = 0;
+  earth.position.y = 0.3;
+  earth.position.z = -15;
+  scene.add(earth);
+});
+
+gltfLoader.load("./textures/work/scene.gltf", (gltf) => {
+  ramen = gltf.scene;
+  ramen.position.set(3,3,3);
+  ramen.scale.set(3,3,3);
+  //scene.add(ramen);
+});
+
+
+
 
 /**
  * 線形補間
@@ -78,49 +211,54 @@ const animationScripts = [];
  */
 animationScripts.push({
   start: 0,
-  end: 40,
+  end: 10,
   function() {
-    camera.lookAt(torus.position);
+    camera.lookAt(earth.position);
     camera.position.set(0, 1, 10);
-    torus.position.z = lerp(-10, 2, scaleParcent(0, 40));
+    earth.position.z = lerp(-10, 2, scaleParcent(0, 40));
+    earth.rotation.x += 0.02;
+    earth.rotation.y += 0.02;
     // console.log(torus.position.z);
   },
 });
 // console.log(animationScripts);
 
 animationScripts.push({
-  start: 40,
-  end: 60,
+  start: 10,
+  end: 20,
   function() {
-    camera.lookAt(torus.position);
-    camera.position.set(0, 1, 10);
-    torus.rotation.z = lerp(0, Math.PI, scaleParcent(40, 60));
+    earth.position.z = lerp(-10, 2, scaleParcent(0, 40));
+    earth.rotation.x += 0.02;
+    earth.rotation.y += 0.02;
+  },
+});
+
+animationScripts.push({
+  start: 20,
+  end: 30,
+  function() {
+    camera.lookAt(earth.position);
+    earth.position.z = lerp(-10, 2, scaleParcent(0, 40));
+    earth.rotation.x += 0.02;
+    earth.rotation.y += 0.02;
     // console.log(torus.position.z);
   },
 });
 
 animationScripts.push({
-  start: 60,
-  end: 80,
-  function() {
-    camera.lookAt(torus.position);
-    camera.position.x = lerp(0, 10, scaleParcent(60, 80));
-    camera.position.y = lerp(1, 12, scaleParcent(60, 80));
-    camera.position.z = lerp(10, 20, scaleParcent(60, 80));
-    // console.log(torus.position.z);
-  },
-});
-
-animationScripts.push({
-  start: 80,
+  start: 30,
   end: 101,
   function() {
-    camera.lookAt(torus.position);
-    torus.rotation.x += 0.02;
-    torus.rotation.y += 0.02;
+    camera.lookAt(earth.position);
+    earth.rotation.x += 0.02;
+    earth.rotation.y += 0.02;
     // console.log(torus.position.z);
   },
 });
+
+
+
+
 
 /**
  * スクロールアニメーション開始
@@ -155,6 +293,7 @@ document.body.onscroll = () => {
 const tick = () => {
   window.requestAnimationFrame(tick);
   playScrollAnimation();
+  shaderMaterial.uniforms.time.value += 0.005;
   renderer.render(scene, camera);
 };
 
